39be5ea885b4850843e5492be95542e0a8eddd32
my282 <myoumyou1228@gmail.com>
2025-10-21 19:31:46 +0900

    in day2 started to edit Main.c

--- Main.c
+++ Main.c
@@ -1,5 +1,23 @@
-#include <Library/UefiLib.h>
+// clang-format off
 #include <Uefi.h>
+
+#include <Library/PrintLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/UefiLib.h>
+#include <Protocol/BlockIo.h>
+#include <Protocol/DiskIo2.h>
+#include <Protocol/LoadedImage.h>
+#include <Protocol/SimpleFileSystem.h>
+// clang-format on
+
+struct MemoryMap {
+  UINTN buffer_size;
+  VOID *buffer;
+  UINTN map_size;
+  UINTN map_key;
+  UINTN descriptor_size;
+  UINT32 descriptor_version;
+};
 
 EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle,
                            EFI_SYSTEM_TABLE *system_table) {
